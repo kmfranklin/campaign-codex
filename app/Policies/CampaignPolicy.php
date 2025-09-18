d4e26629012858b9be0780d8d2969d5e703a6d1b
Kevin Franklin <kevin@franklindesign.io>
2025-09-18 19:01:40 -0400

    feat(policy) add CampaignPolicy stubs for Campaign

---
+++ app/Policies/CampaignPolicy.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Campaign;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class CampaignPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Campaign $campaign): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Campaign $campaign): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Campaign $campaign): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Campaign $campaign): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Campaign $campaign): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can add a member to the campaign.
+     */
+    public function addMember(User $user, Campaign $campaign): bool
+    {
+        // TODO: Check if user is DM of this campaign
+        return false; // Temporary: deny until role checks are implemented
+    }
+
+    /**
+     * Determine whether the user can remove a member from the campaign.
+     */
+    public function removeMember(User $user, Campaign $campaign): bool
+    {
+        // TODO: Check if user is DM of this campaign
+        return false; // Temporary: deny until role checks are implemented
+    }
+}
